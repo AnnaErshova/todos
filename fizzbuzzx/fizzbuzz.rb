40a8f3d22a073ad33acd9e430ba0f4c77ed1ce0d
Katie Hoffman <ktahoffman@gmail.com>
2013-11-15 14:41:02 -0500

    adds fizzbuzzx

---
+++ fizzbuzzx/fizzbuzz.rb
@@ -0,0 +1,15 @@
+101.times do |i|
+
+	if i == 0
+        print ""
+	elsif (i % 3 != 0) && (i % 5 != 0)
+		puts i
+	elsif (i % 3 == 0) && (i % 5 == 0)
+		puts "fizzbuzz"
+	elsif (i % 3 == 0) 
+		puts "fizz"
+	elsif (i % 5 == 0) 
+		puts "buzz"
+	end
+
+end
