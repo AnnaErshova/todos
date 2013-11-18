bb2a334df92c9edf1b295f1d99e01de913ad335d
Katie Hoffman <ktahoffman@gmail.com>
2013-11-18 13:13:01 -0500

    adds refactored fizzbuzz

---
+++ fizzbuzzx/fizzbuzz_refactor.rb
@@ -0,0 +1,28 @@
+def fizzbuzz(num)
+	if (i % 3 != 0) && (i % 5 != 0)
+		puts i
+	elsif (i % 15 == 0)
+		puts "fizzbuzz"
+	elsif (i % 3 == 0) 
+		puts "fizz"
+	elsif (i % 5 == 0) 
+		puts "buzz"
+	end
+end
+
+	
+
+101.times do |i|
+	if i == 0
+        print ""
+	elsif (i % 3 != 0) && (i % 5 != 0)
+		puts i
+	elsif (i % 3 == 0) && (i % 5 == 0)
+		puts "fizzbuzz"
+	elsif (i % 3 == 0) 
+		puts "fizz"
+	elsif (i % 5 == 0) 
+		puts "buzz"
+	end
+
+end
