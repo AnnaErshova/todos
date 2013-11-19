daa9ee5cbe6698e675e59946d9518c2f7a6973fa
Katie Hoffman <ktahoffman@gmail.com>
2013-11-19 15:35:08 -0500

    corrects second section of code

--- programming_lang/Programming Languages.rb
+++ programming_lang/Programming Languages.rb
@@ -56,10 +56,11 @@ new_language_hash = {}
 languages.each do |style, lang_hash|
 	lang_hash.each do |lang_name, attribute_hash|
 		if new_language_hash[lang_name].nil?
-			new_language_hash[lang_name] = {}
+			#if the key language doesn't exist in new hash, so as to avoid overwriting
+			new_language_hash[lang_name] = {} #initiate a new hash for the value of :ruby, :python, etc.
 		end
-		new_language_hash[lang_name][:style] = []
-		new_language_hash[lang_name][:style] << style
+		new_language_hash[lang_name][:style] = [] #creates a key called :style and associates it with an array
+		new_language_hash[lang_name][:style] << style  #adds oo vs functional key from level one to the value of :style
 	end
 end
 
@@ -68,8 +69,9 @@ new_language_hash
 languages.each do |style, lang_hash|
 	lang_hash.each do |lang_name, attribute_hash|
 		attribute_hash.each_pair do |type, interp_or_compliled|
-			if language_attributes[language][attribute].nil?
-				language_attributes[language][attribute] = interp_or_compliled
+			if new_language_hash[language][type].nil?
+				#if the key type doesn't exist in new hash, so as to avoid overwriting
+				new_language_hash[language][type] = interp_or_compliled
 			end
 		end
 	end
