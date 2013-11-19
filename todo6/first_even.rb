54ffb664fa6430efb416e9a92e446d346abc3041
Katie Hoffman <ktahoffman@gmail.com>
2013-11-19 10:40:39 -0500

    adds two different options for solving first_even

--- todo6/first_even.rb
+++ todo6/first_even.rb
@@ -8,3 +8,15 @@ def first_even(array_of_numbers)
 	end
 	
 end
+
+
+# def first_even(array_of_numbers)
+#         even_number_array = []
+#         sorted_array = array_of_numbers.sort
+#         sorted_array.each do |num|              
+#                 if num % 2 == 0
+#                         even_number_array << num
+#                 end
+#         even_number_array[0]
+#         end
+# end
