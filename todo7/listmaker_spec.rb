d1c0002aeb5c055ba86b50ec4c20d92c70439260
Katie Hoffman <ktahoffman@gmail.com>
2013-11-20 09:59:52 -0500

    changes comments on expectations

--- todo7/listmaker_spec.rb
+++ todo7/listmaker_spec.rb
@@ -2,13 +2,13 @@ require './listmaker.rb'
 
 describe "#make_list_a" do
 
-  it "should return the largest number of an array" do
+  it "should return an array of numbered strings" do
     expect(make_list([30, 17, 77])).to be(["1:30", "2:17", "3:77"])
   end
 
 end
 
-describe "#make_list_b" do
+describe "#should return an array of numbered strings with a period and a space" do
 
   it "should return the largest number of an array" do
     expect(make_list([30, 17, 77])).to be(["1. 30", "2. 17", "3. 77"])
