ab8258c9c26affeda98dd3ba36e0c6cf4d1fe3d8
Katie Hoffman <ktahoffman@gmail.com>
2013-11-21 09:21:03 -0500

    adds spec and program for deli to GitHub

--- todo7/listmaker_spec.rb
+++ todo7/listmaker_spec.rb
@@ -1,33 +1,14 @@
 require './listmaker.rb'
 
-describe "#make_list_a" do
-
-	it "should return an array" do
-  		expect(make_list_a([30, 17, 77])).to be_a(Array)
-	end
-
-	it "should return an array of numbered strings" do
-		expect(make_list_a([30, 17, 77])).to eq(["1:30", "2:17", "3:77"])
-	end
-
-	it "should return an array of strings in a numbered list" do
-		expect(make_list_a([2,nil,5])).to eq(["1:2", "2:", "3:5"])
-	end
-
+describe Deli, "#take_a_number" do
+  it "should return an integer" do
+    expect(Deli.new.take_a_number()).to eq(Integer)
+  end
 end
 
-describe "#should return an array of numbered strings with a period and a space" do
 
-	it "should return an array" do
-		expect(make_list_a([30, 17, 77])).to be_a(Array)
-	end
-
-	it "should return the largest number of an array" do
-		expect(make_list_b([30, 17, 77])).to eq(["1. 30", "2. 17", "3. 77"])
-	end
-
-	it "should return an array of strings in a numbered list" do
-		expect(make_list_b([2,nil,5])).to eq(["1. 2", "2. ", "3. 5"])
-	end
-
+describe Deli, "#now_serving" do
+  it "should return an integer" do
+    expect(Deli.new.now_serving()).to eq(Integer)
+  end
 end
