05332687a988eb643cdc39fad4a16cd7fb1e413d
Katie Hoffman <ktahoffman@gmail.com>
2013-11-21 09:21:49 -0500

    adds spec and program for deli to GitHub

---
+++ todo8/deli_counter.rb
@@ -0,0 +1,19 @@
+class Deli
+	attr_accessor :line
+
+	def initialize
+	@line = line
+
+	line = []
+	
+	def take_a_number(name)
+
+		line << "#{index+1}. #{name}"
+	end
+	line
+
+
+	def now_serving(array)
+
+	end
+
