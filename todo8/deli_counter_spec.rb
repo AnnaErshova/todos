bf6211a3c802b95d26c4da606d873270802990bd
Katie Hoffman <ktahoffman@gmail.com>
2013-11-21 09:33:07 -0500

    updates spec

--- todo8/deli_counter_spec.rb
+++ todo8/deli_counter_spec.rb
@@ -1,14 +1,18 @@
 require './deli_counter.rb'
 
 describe Deli, "#take_a_number" do
-  it "should return an integer" do
-    expect(Deli.new.take_a_number()).to eq(Integer)
-  end
+
+	let(:line) {generateCart}
+	before do
+		it "should add blake to the line" do
+			expect(Deli.new.take_a_number(Blake)).to eq(["1. Ashley", "2. Steve", "3. Blake"])
+		end
+	end
 end
 
 
 describe Deli, "#now_serving" do
-  it "should return an integer" do
-    expect(Deli.new.now_serving()).to eq(Integer)
+  it "should remove Ashley from the line" do
+    expect(Deli.new.now_serving(["1. Ashley", "2. Steve", "3. Blake"])).to eq(["2. Steve", "3. Blake"])
   end
 end
