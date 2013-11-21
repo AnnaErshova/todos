d78c768abc9fe4b026a475c616f3a19db873057c
Katie Hoffman <ktahoffman@gmail.com>
2013-11-21 09:46:29 -0500

    corrects program to reflect new variable names

--- todo8/deli_counter.rb
+++ todo8/deli_counter.rb
@@ -2,15 +2,19 @@ class Deli
 	attr_accessor :line
 
 	def initialize
-	@line = []
-
-	def take_a_number(name)
-		line << "1. #{name}"
-		line
+		@line = []
 	end
 
+	def take_a_number(name)
+		if line.nil?
+			line << "1. #{name}"
+		else
+			line << "#{(line.length + 1)}. #{name}"
+		end
+		line
+	end
 
 	def now_serving(array)
 
 	end
-
+end
