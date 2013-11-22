1d337c52a28e2843a329da0ecb6f8a670a94ebb3
Katie Hoffman <ktahoffman@gmail.com>
2013-11-22 09:21:13 -0500

    creates initialize and match methods

---
+++ todo9/anagram.rb
@@ -0,0 +1,39 @@
+class Anagram
+	attr_accessor :word
+
+	def initialize(word)
+		@word = word
+	end
+
+	def match(word)
+
+		if nil?
+			array = []
+		array
+	end
+end
+
+
+  it 'should detect no matches' do
+	detector = Anagram.new('diaper')
+	detector.match(%w(hello world zombies pants)).should eq([])
+  end
+
+  it 'should detect a simple anagram' do
+	detector = Anagram.new('ba')
+	anagrams = detector.match(['ab', 'abc', 'bac'])
+	anagrams.should eq(['ab'])
+  end
+
+  it 'should detect an anagram' do
+	detector = Anagram.new('listen')
+	anagrams = detector.match %w(enlists google inlets banana)
+	anagrams.should eq(['inlets'])
+  end
+
+  it 'should detect multiple anagrams' do
+	detector = Anagram.new('allergy')
+	anagrams = detector.match %w(gallery ballerina regally clergy largely leading)
+	anagrams.should eq(['gallery', 'regally', 'largely'])
+  end
+end
