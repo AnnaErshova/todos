124855836e99eeeab3bc9d0e662a95861a2c0691
Katie Hoffman <ktahoffman@gmail.com>
2013-12-05 09:36:48 -0500

    adds second class to raise error

--- todo14/triangle.rb
+++ todo14/triangle.rb
@@ -12,11 +12,21 @@ class Triangle
 	end
 
 	def kind
-		if s1 == s2 && s2 == s3
+		if s1 <= 0 ||  s2 <= 0 ||  s3 <= 0
+			raise TriangleError
+		elsif s1 + s2 <= s3 || s2 + s3 <= s1 || s3 + s1 <= s2
+			raise TriangleError
+		elsif s1 == s2 && s2 == s3
 			:equilateral
 		elsif s1 == s2 || s2 == s3 || s3 == s1
 			:isosceles
+		else
+			:scalene
 		end
 	end
+end
 
+# Nikki's idea:
+class TriangleError < StandardError
+	"ERROR ERROR ERROR!!!!!!!"
 end
