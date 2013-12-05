9f5d1f08569755ac635a0b3496494838f0b39a60
Katie Hoffman <ktahoffman@gmail.com>
2013-12-05 09:18:50 -0500

    adds kind defintion

--- todo14/triangle.rb
+++ todo14/triangle.rb
@@ -3,7 +3,15 @@
 ########################
 
 class Triangle
+	attr_reader :s1, :s2, :s3
+
 	def initalize(s1, s2, s3)
+		@s1 = s1
+		@s2 = s2
+		@s3 = s3
+	end
+
+	def kind
 		if s1 == s2 && s2 == s3
 			:equilateral
 	end
