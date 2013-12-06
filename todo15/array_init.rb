98cf810c45e7244f370076284f35025d40e14348
Katie Hoffman <ktahoffman@gmail.com>
2013-12-06 09:48:10 -0500

    passes all 7 tests

--- todo15/array_init.rb
+++ todo15/array_init.rb
@@ -2,18 +2,17 @@
 #### A R R A Y ####
 ###################
 
-def array_init(items = 5)
-	if block_given? = false # see below :
-		array = Array.new(items) 
-		{ |i| yield i } 
-	else
-		array = Array.new(items) 
-		{ |i| (100 * i) }
-	end
-	
-end
-
-# http://apidock.com/ruby/Kernel/block_given%3F : 
+def array_init(parameter = 5)
+	array = [] 
+	if block_given? == false # see below ================>===
+		parameter.times { |i| array << (100 * i).to_s }		#
+	else													#
+		parameter.times { |i| array << yield(i)} 			#
+	end														#
+	array													#
+end														    #
+															#
+# http://apidock.com/ruby/Kernel/block_given%3F : <=========#
 # def try
 #   if block_given?
 #     yield
