86e4ff633854450c4f24edc386d45d4ab31f9953
Katie Hoffman <ktahoffman@gmail.com>
2013-12-06 14:41:10 -0500

    adds new tests, program passes all tests

--- pig_latin/pig_latin.rb
+++ pig_latin/pig_latin.rb
@@ -3,8 +3,34 @@
 #################
 
 class PigLatin
-	attr_reader :sting
+	attr_reader :string
 	def initialize(string)
 		@string = string
+	end
+
+	def parse_string
+		string.split
+	end
+
+	def apply_rules
+		parse_string.collect do |s|
+			if s.length > 1
+				if s[0..1] == "sh" || s[0..1] == "th" ||s[0..1] == "qu"
+					first_letters = s[0..1]
+					string = s[2..-1]
+					"#{string}#{first_letters}ay"
+				else
+					first_letter = s[0]
+					string = s[1..-1]
+					"#{string}#{first_letter}ay"
+				end
+			else
+				"#{s}ay"
+			end
+		end
+	end
+
+	def array_to_string
+		apply_rules.join(" ")
 	end
 end
--- pig_latin/pig_latin_spec.rb
+++ pig_latin/pig_latin_spec.rb
@@ -1,7 +1,9 @@
 #####################################
 ### P I G    L A T I N   S P E C  ###
 #####################################
-require './pig_latin.rb'
+
+require './pig_latin'
+
 RSpec.configure do |config|
 	config.color_enabled = true
 	config.tty = true
@@ -14,8 +16,38 @@ describe PigLatin, "#initialize" do
  	end
 end
 
-describe PigLatin, "#method" do
-  it "should return an integer" do
-    expect(PigLatin.new.method).to be_a("string")
-  end
+describe PigLatin, "#parse_string" do
+	it "should return an array" do
+		piglatin = PigLatin.new("banana nut squash")
+		expect(piglatin.parse_string).to eq(["banana", "nut", "squash"])
+	end
+	it "should " do
+		piglatin = PigLatin.new("shut that quiet mouth")
+		expect(piglatin.parse_string).to eq(["shut", "that", "quiet", "mouth"])
+	end
 end
+
+describe PigLatin, "#apply_rules" do
+	it "should return an array with modified strings" do
+		piglatin = PigLatin.new("banana nut squash")
+		expect(piglatin.apply_rules).to eq(["ananabay", "utnay", "quashsay"])
+	end
+	it "should not raise error when a word with only one letter is entered" do
+		piglatin = PigLatin.new("i am a very important secret")
+		expect(piglatin.apply_rules).to eq(["iay", "maay", "aay", "eryvay", "mportantiay", "ecretsay"])
+	end
+	it "should put 'sh' and 'th' and 'qu' at the end to maintain sound" do
+		piglatin = PigLatin.new("shut that quiet mouth")
+		expect(piglatin.apply_rules).to eq(["utshay", "atthay", "ietquay", "outhmay"])
+	end
+end
+
+describe PigLatin, "#array_to_string" do
+	it "should return a string with modifications" do
+		piglatin = PigLatin.new("banana nut squash")
+		expect(piglatin.array_to_string).to eq("ananabay utnay quashsay")
+	end
+end
+
+
+
