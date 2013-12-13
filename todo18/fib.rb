ce521d224cb867e7ebf1c0a776da7372f970981a
Katie Hoffman <ktahoffman@gmail.com>
2013-12-13 15:15:55 -0500

    adds another method that calls on itself to get the number

--- todo18/fib.rb
+++ todo18/fib.rb
@@ -1,31 +1,21 @@
-# prereqs: arrays, methods, conditional logic
-# create a method that returns the nth number of the fibonacci sequence
-# fib sequence = 0,1,1,2,3,5,8,13 etc, assume the 1st element is 0 (the first number), the 2nd element is 1, the third element is 1
-########################################################################################################
-# from Stack Overflow:
-def fibo_finder(n)
-	hash = Hash.new do |hash,key| 
-		if hash[key] = key < 2
-			key
-		else
-			hash[key-1] + hash[key-2]
-		end
-	end
-	hash[n]
-end
+#############
+### F I B ###
+#############
 
-# Resources that helped me to understand the above method
-#  x(n) = x(n-1) + x(n-2)
-# h = Hash.new { |hash, key| hash[key] = "Go Fish: #{key}" }
-# h["c"]           #=> "Go Fish: c"
-# h["c"].upcase!   #=> "GO FISH: C"
-# h["d"]           #=> "Go Fish: d"
-# h.keys           #=> ["c", "d"]
 ########################################################################################################
-
 # My terribly hacky solution for the spec
-
 # def fibo_finder(n)
 # 	array = [0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181,6765]
 # 	return array[n]
 # end
+########################################################################################################
+# from meowist.github
+def fibo_finder(n)
+  if n <= 1
+  	n
+  else
+    fibo_finder(n-1) + fibo_finder(n-2)
+  end
+end
+# this method uses recursion, which is a concept that kinda blows my mind
+########################################################################################################
