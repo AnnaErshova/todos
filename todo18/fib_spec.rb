f99b657b1befd92e8d60829a6cc879132bd897ca
Katie Hoffman <ktahoffman@gmail.com>
2013-12-13 15:00:58 -0500

    adds config settings to spec for readability

--- todo18/fib_spec.rb
+++ todo18/fib_spec.rb
@@ -1,5 +1,16 @@
 require "./fib.rb"
 
+RSpec.configure do |config|
+  # Use color in STDOUT
+  config.color_enabled = true
+
+  # Use color not only in STDOUT but also in pagers and files
+  config.tty = true
+
+  # Use the specified formatter
+  config.formatter = :documentation # :progress, :html, :textmate
+end
+
 describe "fibo finder" do
   
   it "it should return the nth number of the fibonacci sequence" do
