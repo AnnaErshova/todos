0770df13d3b84c2161d905b371ee0712596bee79
Katie Hoffman <ktahoffman@gmail.com>
2013-12-17 15:47:48 -0500

    adds functioning find_by_id method

--- dog_sql_activity/my_dog.rb
+++ dog_sql_activity/my_dog.rb
@@ -1,26 +1,43 @@
 require "Mysql2"
 
 class Dog
-	attr_accessor :color, :name
-	attr_reader :id
+	attr_accessor :color, :name, :id
 
-	@@db = MyMysql2::Client.new(
+	@@db = Mysql2::Client.new(
 		  :host => "127.0.0.1",
 		  :username => "root",
 		  :database => "dogs"
 		  )
 
-	def initialize
-		@color = color
+	def initialize(name, color, id=nil)
+		@id = id
 		@name = name
+		@color = color
 	end
 
-	def self.insert
-	self.deb.query("
-		INSERT INTO dogs (name, color)
-		values ('#{self.name}', '#{self.color}')
-		")
-	self.mark_as_saved!
+	def self.db
+		@@db
+	end
+
+	def db
+		@@db
+	end
+
+	def self.find_by_id(id)
+		results = db.query("
+			SELECT *
+			FROM dogs
+			WHERE id = #{id}
+			")
+		results.first
+	end
+
+	def self.new_from_db(row)
+		dog = Dog.new(row[:name], row[:color], row[:id])
+		dog
 	end
 
 end
+
+dog_a = Dog.find_by_id(1)
+puts dog_a.inspect
