672e379e9444af47d7635b7782d4ed087b0a180e
Katie Hoffman <ktahoffman@gmail.com>
2013-12-17 14:24:57 -0500

    makes better funtioning program, refactors, accounts for words that begin with vowels

--- pig_latin/pig_latin.rb
+++ pig_latin/pig_latin.rb
@@ -3,9 +3,11 @@
 #################
 
 class PigLatin
-	attr_reader :string
+	attr_reader :string, :punctuation
+
 	def initialize(string)
-		@string = string
+		@string = string[0..-2]
+		@punctuation = string[-1]
 	end
 
 	def parse_string
@@ -15,10 +17,19 @@ class PigLatin
 	def apply_rules
 		parse_string.collect do |s|
 			if s.length > 1
-				if s[0..1] == "sh" || s[0..1] == "th" ||s[0..1] == "qu"
+				if  s[0..1].downcase == "sh"  || 
+					s[0..1].downcase == "th"  ||
+					s[0..1].downcase == "qu"
 					first_letters = s[0..1]
 					string = s[2..-1]
 					"#{string}#{first_letters}ay"
+				elsif   s.downcase == "is" 	    || s.downcase == "sigh"    || 
+						s.downcase == "my"      || s.downcase == "by"      ||
+						s.downcase == "buy"     || s[0].downcase == "i"    ||
+						s[0].downcase == "a"	|| s[0].downcase == "e"    ||
+						s[0].downcase == "o"    || s[0].downcase == "u"    || 
+						s[0].downcase == "y"
+					"#{s}ay"				
 				else
 					first_letter = s[0]
 					string = s[1..-1]
@@ -31,6 +42,10 @@ class PigLatin
 	end
 
 	def array_to_string
-		apply_rules.join(" ")
+		pig_latin = apply_rules.join(" ").capitalize
+		pig_latin.insert(-1, punctuation)
 	end
 end
+
+latin = PigLatin.new("This is a long string that I will enter!")
+puts latin.array_to_string
