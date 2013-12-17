0efd5a0fa8d08af3a379ac1efd6a7e429016a20a
Katie Hoffman <ktahoffman@gmail.com>
2013-12-17 09:17:46 -0500

    adds non-funtioning uniq method

--- todo21/unique.rb
+++ todo21/unique.rb
@@ -1,14 +1,22 @@
-# instructions: implement Array.uniq
-
+###############
+### U N I Q ###
+###############
 class Array
 	attr_accessor :array
 
 	def initialize(array)
 		@array = array
 	end
-		
+
 	def uniq
-		array.uniq
+		container = []
+		array.each do |element|
+			array.collect do |item|
+				if element == item
+					container << element
+				end
+			end
+		end
 	end
 
 end
