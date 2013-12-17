67470e972286f361f1ea74dfcb27690a45d4710e
Katie Hoffman <ktahoffman@gmail.com>
2013-12-17 10:11:25 -0500

    makes a better method, way more concise

--- todo21/unique.rb
+++ todo21/unique.rb
@@ -2,59 +2,12 @@
 ### U N I Q ###
 ###############
 class Array
-	attr_accessor :array, :num, :count, :hash
-
-	def initialize(array)
-		@array = array
-		@length = array.count
-		@num = 0
-		@hash = Hash.new
-		@new_array = Array.new
-	end
-
-	def find_length
-		if length %2 == 0
-			num = length / 2
-		else
-			num = (length + 1) / 2
-		end
-		length
-	end
-
-	def add_count
-		count = 0
-		lenth.times do
-			when count <= num
-				new_array << array[count]
-				new_array << {:count => 1}
-				count += 1
-			end
+	def uniq
+	array = []
+	self.each do |element| 
+		if array.include?(element) == false
+			array << element 
 		end
 	end
-
-	def to_hash
-		hash = Hash[*new_array]
-	end
-
-	def count_items
-		array.each do |item|
-        	hash.each do |element, count_hash|
-        		if item == element
-					count_hash[:count] += 1
-            	end
-			end
-		end
-		hash
-	end
-
-	def delete_items
-		hash.delete_if do |element, count_hash|
-			count_hash.each {|key, value| value >= 2}
-		end
-		hash
-	end
-
-	def add_back_items
-		#add back items that got deleted
-	end
+	array
 end
---
+++ todo21/unique_spec.rb
@@ -0,0 +1,8 @@
+require './unique.rb'
+
+describe "unique" do
+  it "should return only one copy of a redundant array" do
+    array = [1, 1, 1, 2, 3, 4, 5, 4]
+    expect(array.uniq).to eq([1, 2, 3, 5, 4])
+  end
+end
