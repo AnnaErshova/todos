5c023257c12ebdcc876a6c82532f310ecd6858e1
Katie Hoffman <ktahoffman@gmail.com>
2013-12-18 15:46:06 -0500

    adds functioning caluculator

--- todo22/lib/birthday.rb
+++ todo22/lib/birthday.rb
@@ -6,20 +6,23 @@ require 'time'
 
 class Birthday
 	attr_accessor :time, :birthday
-	def initialize(yyyymmdd)	# for instance: Birthday.new("19890618")
+
+	def initialize(bd_yyyymmdd)	# for instance: Birthday.new("19890618")
 		#####################################
 		@time = Time.now.strftime("%Y%m%d")	# today this will return "20131218"
 		#####################################
 		this_year = time[0..3]	# gets today's year
-		i_year = t_yyy.to_i	# gets next year part 1/3
+		i_year = this_year.to_i	# gets next year part 1/3
 		year = i_year + 1	# gets next year part 2/3
 		next_year = year.to_s	# gets next year part 3/3
 		this_month = time[4..5]		# gets today's month
 		this_day = time[-2..-1]		# gets today's day
 
-		bd_mmdd = yyyymmdd[4..-1]	# strip year off birthday date
+		bd_mmdd = bd_yyyymmdd[4..-1]	# strip year off birthday date
+		bd_yyyy = bd_yyyymmdd[0..3] # gets birthday year
 		bd_mm = bd_mmdd[0..1]	# gets birthday month
 		bd_dd = bd_mmdd[-2..-1]		# gets birthday day
+
 		
 		if bd_mm > this_month
 			@birthday = bd_mmdd.insert(0, this_year)
@@ -29,28 +32,39 @@ class Birthday
 			else									
 				@birthday = bd_mmdd.insert(0, next_year)
 			end
-		elsif bd_mm < this_month
-			@birthday = mmdd.insert(0, next_year)
+		else # bd_mm < this_month
+			@birthday = bd_mmdd.insert(0, next_year)
 		end
 		###########################################
 		@birthday # Birthday.new("19890618")[@birthday] => "20140618"
 		###########################################
-		end
 	end
 
 	def birthday?
-		if time == birthday
+		if @time == @birthday
 			return "You're birthday is today. Happy birthday!"
 		else
-			t = Date.parse(time)
-			b = Date.parse(birthday)
+			t = Date.parse(@time)
+			b = Date.parse(@birthday)
 			wait_1 = (t.mjd - b.mjd).abs 	# mjd makes it an integer, abs is absolute value
 			wait_2 = (b.mjd - t.mjd).abs
 			if wait_1 <= wait_2
-				"Your birthday is in #{wait_1} day(s)."
+				if wait_1 > 1
+					"Your birthday is in #{wait_1} days."
+				else
+					"Your birthday is tomorrow!"
+				end
 			else
-				"Your birthday is in #{wait_2} days(s)."
+				if wait_2 > 1
+					"Your birthday is in #{wait_1} days."
+				else
+					"Your birthday is tomorrow!"
+				end
 			end
 		end
 	end
 end
+
+
+my_bd = Birthday.new("19891219")
+puts my_bd.birthday?.inspect
