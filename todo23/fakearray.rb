137dc9a11d9078e1a1566dbcb57554e708266a65
Katie Hoffman <ktahoffman@gmail.com>
2013-12-19 09:35:29 -0500

    completes init method and each method

--- todo23/fakearray.rb
+++ todo23/fakearray.rb
@@ -1,35 +1,33 @@
-To explore the confusion around the results object we get back from queries using the mysql2 gem...
+# To explore the confusion around the results object we get back from queries using the mysql2 gem...
 
-Create an object that behaves like an array but is NOT an array. Define the each, first and indexing methods on it.
+# Create an object that behaves like an array but is NOT an array. Define the each, first and indexing methods on it.
 
-I should get the same result if i do
+# I should get the same result if i do
 
-container = []
-[1,2,3].each do |element|
-  container << element + 1
-end
+# container = []
+# [1,2,3].each { |element| container << element + 1 }
 
-as if i use your class like so...
+# as if i use your class like so...
 
-fakearray = FakeArray.new
-container = []
-fakearray.each do |element|
-  container << element + 1
-end
+# fakearray = FakeArray.new
+# container = []
+# fakearray.each do |element|
+#   container << element + 1
+# end
 
-Note: I've intentionally omitted a step here where you'd have to somehow tell your class that were using an array of 1,2,3
+# Note: I've intentionally omitted a step here where you'd have to somehow tell your class that were using an array of 1,2,3
 
 class FakeArray
-	NUM_1 = 1
-	NUM_2 = 2
-	NUM_3 = 3
-
-	attr_accessor :items
-	def initialize
-		
+	attr_accessor :ele_1, :ele_2, :ele_3
+	def initialize(ele_1, ele_2, ele_3)
+		@ele_1 = ele_1
+		@ele_2 = ele_2
+		@ele_3 = ele_3
 	end
 	def each
-
+		yield ele_1
+		yield ele_2
+		yield ele_3
 	end
 
 	def first
