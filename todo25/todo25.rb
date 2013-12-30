4e15ffab6acd8a71cbfb17af8c8fc880eae57de1
Katie Hoffman <ktahoffman@gmail.com>
2013-12-30 11:11:37 -0500

    adds todo25

---
+++ todo25/todo25.rb
@@ -0,0 +1,7 @@
+Troubleshooting "Embroidery Trouble Shooting Guide"
+
+The issue with this page is that the h3s are never ended, resulting in nested header tags. This nesting produces the ever-increasing font size.
+
+The website started in between Jan. 1997 to 2004. I get these dates because it is HTML3.2 (released in Jan. 1997) and it was archived in 2004. To get more precise, it might have started between Jan. 1997 and Dec. 1997 because then HTML4 was introduced. Of course, it is possible that the developer intentionally used 3.2 even after the introduction of 4. However, the domain was registered in 2003 and it has been updated in 2012. WHA???
+
+A primary criticism of the page is that it is really just a list of problems but does not really help the embroiderer to diagnose problems.
