6c9323a168225624ed356bd76dce4e6bf3b162da
Katie Hoffman <ktahoffman@gmail.com>
2014-01-08 09:20:08 -0500

    adds attempt at palindrome activity

---
+++ todo31/palindrome.rb
@@ -0,0 +1,12 @@
+class String
+  def palindrome?(string)
+  	reversed = string.reverse
+    max = string.length
+    string.each_with_index do |item, index|
+    	if index < max
+    		?????
+    	end
+    end
+
+  end
+end
