728b69754f01614e25998b49b9297e233b87996d
Katie Hoffman <ktahoffman@gmail.com>
2014-01-08 09:34:54 -0500

    adds methods

--- todo31/palindromes.rb
+++ todo31/palindromes.rb
@@ -1,6 +1,24 @@
 A palindrome is a string that is written the same forward as it is in reverse. Write a method to return the longest palindrome in a given string
 
-def longest_palindrome
+class Palindrome
+	attr_reader :string
+
+	def initialize(sting)
+		@string = string
+	end
+
+	def increment
+		max = string.length
+		counter = 0
+		while i < max
+			get_palindrome(i)
+		end
+	end
+
+	def get_palindrome(i)
+		
+	end
+
 
 end
 
