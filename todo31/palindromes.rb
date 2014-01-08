63321591f2d6f6d4924edb4cfcf534912e8f7d6f
Katie Hoffman <ktahoffman@gmail.com>
2014-01-08 10:11:34 -0500

    adds get_palindrome to class

--- todo31/palindromes.rb
+++ todo31/palindromes.rb
@@ -7,16 +7,23 @@ class Palindrome
 
 	def increment(string)
 		max = string.length
-		counter = 0
+		i = 0
 		while i < max
 			get_palindrome(i)
+			i += 1
 		end
 	end
 
-	def get_palindrome(i)
-		if string[i-1] == string[i+1]
-
+	def get_palindrome(i, string)
+		array = []
+		array << string[i]
+		counter = 1
+		while string[i-counter] == string[i+counter]
+			array << string[i-counter]
+			counter += 1
 		end
+		array
+		array.insert(0, array[1..-1].reverse)
 	end
 end
 
