4a95da913b2ea7e1bb7f42b38aa429ac7475a9a1
Katie Hoffman <ktahoffman@gmail.com>
2014-01-13 09:25:55 -0500

    adds breeds that have many puppies

---
+++ todo34/nested_rails_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NestedRailsApp::Application.config.secret_token = '0fdfe3fd557ce0f21cc1823b7076b5ca2375ca3c2b977526030739c6560e2cb9829a599dad7d2c90750993b11f3a75baf965330e975b2403942ba2d1efdea59e'
