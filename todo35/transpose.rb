73b02ad5e460c7dffa73e88395e44be6dc934804
Katie Hoffman <ktahoffman@gmail.com>
2014-01-14 09:42:40 -0500

    adds semi-functioning method

--- todo35/transpose.rb
+++ todo35/transpose.rb
@@ -5,21 +5,21 @@ class Array
 	end
 	def my_transpose
 		return arr if arr.empty?
-		new_array = []
-		counter = 0
-		num = arr.length
-		num.times do
-			arr.each do |mini_array|
-				new_array = []
-				new_mini_array <<mini_array[counter]
-				new_array << new_mini_array
-				counter += 1
+		new_array = Array.new
+		arr[0].length.times { new_array << [] }
+		index_array = 0
+		arr.each do |mini_array|
+			index_item = 0
+			mini_array.each do |item|
+				new_array[item_index][arr_index] = item
+				item_index += 1
 			end
+			arr_index += 1
 		end
 		new_array
 	end
 end
 
-my_array = Array.new([[1, 2, 3], [:a, :b, :c]])
-result = my_array.my_transpose
+mine = Array.new([[1, 2, 3], [:a, :b, :c]])
+result = mine.my_transpose
 puts result.inspect
