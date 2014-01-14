bd2ac6e4f8b307c7490c60f7f0fb8d9d29805500
Katie Hoffman <ktahoffman@gmail.com>
2014-01-14 09:28:38 -0500

    adds non-functioning method

--- todo35/transpose.rb
+++ todo35/transpose.rb
@@ -1,5 +1,25 @@
 class Array
-  def my_transpose
-    ___
-  end
+	attr_reader :arr
+	def initialize(arr)
+		@arr = arr
+	end
+	def my_transpose
+		return arr if arr.empty?
+		new_array = []
+		counter = 0
+		num = arr.length
+		num.times do
+			arr.each do |mini_array|
+				new_array = []
+				new_mini_array <<mini_array[counter]
+				new_array << new_mini_array
+				counter += 1
+			end
+		end
+		new_array
+	end
 end
+
+my_array = Array.new([[1, 2, 3], [:a, :b, :c]])
+result = my_array.my_transpose
+puts result.inspect
