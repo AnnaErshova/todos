a4a1c2ef7111349724b0cb2ede1c0f156e4e61c2
Katie Hoffman <ktahoffman@gmail.com>
2014-01-20 09:18:08 -0500

    adds attempt to complete without regex

--- todo40/comma.rb
+++ todo40/comma.rb
@@ -1,3 +1,22 @@
-def separate_with_comma(n)
-  num = n.to_s.reverse.gsub(/...(?=.)/,'\&,').reverse
+# def separate_with_comma(n)
+#   n.to_s.reverse.gsub(/...(?=.)/,'\&,').reverse
+# end
+
+def separate_with_comma(n) # (10,000)
+  string = n.to_s
+  length = string.length # 5
+
+  num_of_commas = length / 3
+  num_of_arrays = num_of_commas + 1
+
+  new_array = []
+  num_of_arrays.times { new_array << [] }
+
+
+  counter = 0
+  num_of_commas.times do
+    << n[-1..-3]
+
+  end
+
 end
