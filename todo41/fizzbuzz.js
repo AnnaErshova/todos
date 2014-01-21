1d0b532d37a5d8f9e587821320088d76fae37885
Katie Hoffman <ktahoffman@gmail.com>
2014-01-21 09:26:02 -0500

    updates syntax so that it does not raise errors

--- todo41/fizzbuzz.js
+++ todo41/fizzbuzz.js
@@ -1,5 +1,5 @@
 for (var i = 1; i <= 100; i++) {
-  if !(i % 5 == 0) && !(i % 3 == 0) {
+  if (i % 5 !== 0 && i % 3 !== 0) {
     console.log(i);
   } else if (i % 15 == 0) {
     console.log("fizzbuzz");
